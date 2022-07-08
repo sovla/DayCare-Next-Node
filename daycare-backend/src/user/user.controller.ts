95808b462d2edcb7a945c75573449733aaef5f0d
sovla <41351496+sovla@users.noreply.github.com>
2022-07-08 19:34:18 +0900

    Add Comment Controller

--- daycare-backend/src/user/user.controller.ts
+++ daycare-backend/src/user/user.controller.ts
@@ -20,21 +20,25 @@ export class UserController {
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
+    // 회원가입
     return this.userService.create(createUserDto);
   }
 
   @Post('/email')
   verifyEmail(@Body() email: string) {
+    // 이메일 인증 및 중복 확인
     return this.userService.verifyEmail(email);
   }
 
   @Delete()
   removeUser(@Req() req: Request) {
+    // 회원탈퇴
     return this.userService.removeUser(req);
   }
 
   @Patch()
   updateUser(@Req() req: Request, @Body() updateUserDto: UpdateUserDto) {
+    // 유저 업데이트
     return this.userService.update(req, updateUserDto);
   }
 }
