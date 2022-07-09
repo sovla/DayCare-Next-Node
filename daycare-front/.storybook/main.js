bc6ae46e0de6e2b5186685dcbc46bdfb3b34e551
sovla <41351496+sovla@users.noreply.github.com>
2022-07-09 23:16:57 +0900

    Add StoryBook Addon [접근성,객체다루기,뷰포트]

--- daycare-front/.storybook/main.js
+++ daycare-front/.storybook/main.js
@@ -1,15 +1,29 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
-}
+    stories: [
+        "../stories/**/*.stories.mdx",
+        "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+    ],
+    addons: [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/addon-interactions",
+        "@storybook/addon-a11y/register",
+        "@storybook/addon-knobs/register",
+        "@storybook/addon-viewport/register",
+    ],
+    framework: "@storybook/react",
+    core: {
+        builder: "@storybook/builder-webpack5",
+    },
+    typescript: {
+        check: false, // 선택적으로 fork-ts-checker-webpack-plugin 실행
+        checkOptions: {}, // 선택적으로 fork-ts-checker-webpack-plugin 실행
+        reactDocgen: "react-docgen-typescript", // 실행할 반응 docgen 프로세서: "react-docgen-typescript", "react-docgen",false
+        reactDocgenTypescriptOptions: {
+            // react-docgen-typescript가 활성화된 경우 react-docgen-typescript-plugin에 전달할 옵션입니다.
+            shouldExtractLiteralValuesFromEnum: true,
+            propFilter: (prop) =>
+                prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
+        },
+    },
+};
