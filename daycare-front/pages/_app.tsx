f6d1e2663144a4b6f01513472bbd1e16ff254410
sovla <41351496+sovla@users.noreply.github.com>
2022-07-12 11:00:29 +0900

    feat(redux-toolkit): 리덕스 툴킷 설치 및 기본 셋팅 완료

--- daycare-front/pages/_app.tsx
+++ daycare-front/pages/_app.tsx
@@ -1,8 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+/* eslint-disable react/jsx-props-no-spreading */
+
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import wrapper from '@src/Store/store';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default wrapper.withRedux(MyApp);
---
+++ daycare-front/src/Store/store.ts
@@ -0,0 +1,20 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { createWrapper } from 'next-redux-wrapper';
+
+const makeStore = configureStore({
+  reducer: {
+    // the convention is to name this photos rather
+    // than photosStore but photosStore is clearer to me.
+    // anyOtherStore: anyOtherSlice,
+    // middleware: ['array of middlewares'],
+  },
+  devTools: true,
+});
+
+const wrapper = createWrapper(() => makeStore);
+
+export default wrapper;
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof makeStore.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof makeStore.dispatch;
