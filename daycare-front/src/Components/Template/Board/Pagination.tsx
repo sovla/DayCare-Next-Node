7a0390576c7dd2393d98aaaa0d60ac37c0236406
sovla <41351496+sovla@users.noreply.github.com>
2022-07-14 00:19:18 +0900

    fix(template/pagination): 디자인 수정 및 Bootstrap 제거

--- daycare-front/src/Components/Template/Board/Pagination.tsx
+++ daycare-front/src/Components/Template/Board/Pagination.tsx
@@ -1,10 +1,50 @@
 /* eslint-disable react/no-array-index-key */
+import Theme from '@src/assets/global/Theme';
 import { PaginationProps } from '@src/Type/Template/Board';
 import React, { useMemo } from 'react';
-import BSRPagination from 'react-bootstrap/Pagination';
+import styled from 'styled-components';
 
-//  reset css 방지
-import './css/Pagination.css';
+const PaginationUl = styled.ul`
+  display: flex;
+  & > li:first-of-type > button {
+    border-radius: 5px 0px 0px 5px;
+  }
+
+  & > li:last-of-type > button {
+    border-radius: 0px 5px 5px 0px;
+  }
+  & > li:nth-of-type(-n + 2) > button {
+    padding-bottom: 3px;
+    font-size: 16px;
+  }
+  & > li:nth-last-of-type(-n + 2) > button {
+    padding-bottom: 3px;
+    font-size: 16px;
+  }
+`;
+const PageButton = styled.button<{ active?: boolean }>`
+  width: 30px;
+  height: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 0.2px solid ${Theme.color.gray_C1};
+
+  color: ${(p) => {
+    if (p.disabled) {
+      return Theme.color.gray_99;
+    }
+    if (p.active) {
+      return '#ffffff';
+    }
+    return Theme.color.blue_00;
+  }};
+  transition: 0.3s;
+  background-color: ${(p) => (p.active ? Theme.color.blue_00 : '#ffffff')};
+  &:hover {
+    background-color: ${(p) => (p.disabled ? '#ffffff' : Theme.color.gray_D9)};
+  }
+`;
 
 const Pagination: React.FC<PaginationProps> = (props) => {
   const { maxPage, onClickPage, selectPage } = props;
@@ -20,34 +60,61 @@ const Pagination: React.FC<PaginationProps> = (props) => {
   }, [selectPage]);
 
   return (
-    <BSRPagination size="sm">
-      <BSRPagination.First
-        disabled={selectPage === 1}
-        onClick={() => onClickPage(1)}
-      />
-      <BSRPagination.Prev
-        disabled={selectPage - 5 < 1}
-        onClick={() => onClickPage(selectPage - 5)}
-      />
-      {pageArray.map((v, i) => {
+    <PaginationUl>
+      <li>
+        <PageButton
+          type="button"
+          disabled={selectPage === 1}
+          onClick={() => onClickPage(1)}
+        >
+          «
+        </PageButton>
+      </li>
+      <li>
+        <PageButton
+          type="button"
+          disabled={selectPage - 5 < 1}
+          onClick={() => onClickPage(selectPage - 5)}
+        >
+          ‹
+        </PageButton>
+      </li>
+
+      {pageArray.map((v) => {
         if (v > maxPage) {
           return null;
         }
         return (
-          <BSRPagination.Item active={selectPage === v} key={i}>
-            {v}
-          </BSRPagination.Item>
+          <li key={v}>
+            <PageButton
+              onClick={() => onClickPage(v)}
+              active={selectPage === v}
+              type="button"
+            >
+              {v}
+            </PageButton>
+          </li>
         );
       })}
-      <BSRPagination.Next
-        disabled={selectPage + 5 > maxPage}
-        onClick={() => onClickPage(selectPage + 5)}
-      />
-      <BSRPagination.Last
-        disabled={selectPage === maxPage}
-        onClick={() => onClickPage(maxPage)}
-      />
-    </BSRPagination>
+      <li>
+        <PageButton
+          type="button"
+          disabled={selectPage + 5 > maxPage}
+          onClick={() => onClickPage(selectPage + 5)}
+        >
+          ›
+        </PageButton>
+      </li>
+      <li>
+        <PageButton
+          type="button"
+          disabled={selectPage === maxPage}
+          onClick={() => onClickPage(maxPage)}
+        >
+          »
+        </PageButton>
+      </li>
+    </PaginationUl>
   );
 };
 
