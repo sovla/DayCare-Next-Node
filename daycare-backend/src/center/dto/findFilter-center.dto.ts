e74492a2c929495680bbdcbfd72b416906730bf9
sovla <41351496+sovla@users.noreply.github.com>
2022-07-20 22:02:03 +0900

    fix(center): dto Type 변경

--- daycare-backend/src/center/dto/findFilter-center.dto.ts
+++ daycare-backend/src/center/dto/findFilter-center.dto.ts
@@ -1,13 +1,42 @@
+import { Center } from './../../domain/center.entity';
 export class FindFilterDTO {
   lat: string;
   lon: string;
-  type: string | null;
+  type: string | undefined;
   city: string | null;
   city_detail: string | null;
-  max: boolean | null;
-  property: string | null;
-  empty_class: string | null;
-  employee_count: number | null;
-  employee: string | null;
-  radius: number | null;
+  max: string | null;
+  property:
+    | 'property_after'
+    | 'property_after_combine'
+    | 'property_allday'
+    | 'property_holiday'
+    | 'property_impaired'
+    | 'property_impaired_combine'
+    | 'property_infants'
+    | 'property_normal'
+    | 'property_time_extension'
+    | undefined;
+  empty_class:
+    | 'child_count_age0'
+    | 'child_count_age1'
+    | 'child_count_age2'
+    | 'child_count_age3'
+    | 'child_count_age4'
+    | 'child_count_age5'
+    | undefined;
+  employee_count: string | null;
+  employee:
+    | 'employee_count_cook'
+    | 'employee_count_director'
+    | 'employee_count_cure'
+    | 'employee_count_special'
+    | 'employee_count_other'
+    | 'employee_count_officeworker'
+    | 'employee_count_nutritionist'
+    | 'employee_count_nursingassistant'
+    | 'employee_count_nursery'
+    | 'employee_count_nurse'
+    | undefined;
+  radius: string | null;
 }
