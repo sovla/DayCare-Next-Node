0143ed8e29a7f7a25bb15165a14c094c12385b09
sovla <41351496+sovla@users.noreply.github.com>
2022-08-05 15:11:31 +0900

    feat(userupdate): head태그 내용 추가

--- daycare-front/src/Container/Page/UserUpdate.tsx
+++ daycare-front/src/Container/Page/UserUpdate.tsx
@@ -10,6 +10,7 @@ import { changeUser, selectUser } from '@src/Store/userState';
 import { getUserInformationType, userPatchType } from '@src/Type/API/user';
 import RegExp from '@src/Util/RegExp';
 import { AxiosResponse } from 'axios';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import React, { useCallback, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
@@ -131,6 +132,11 @@ const UserUpdate: React.FC<{
   }
   return (
     <StyledDiv>
+      <Head>
+        <title>DayCare</title>
+        <meta name="description" content="DayCareUser" />
+        <link rel="icon" href="/LogoIcon.png" />
+      </Head>
       <nav className="nav-menu">
         <Menu />
       </nav>
