4eea1d9089ba17e39347bedb82fe0dfd159b9e64
sovla <41351496+sovla@users.noreply.github.com>
2022-08-15 13:28:00 +0900

    fix(jwt): jwt guard 구조 변경

--- daycare-backend/src/review/guard/jwt.guard.ts
+++ daycare-backend/src/review/guard/jwt.guard.ts
@@ -6,39 +6,24 @@ import {
   HttpException,
 } from '@nestjs/common';
 import { Request } from 'express';
-import { Observable } from 'rxjs';
 import { jwtUserDTO } from 'src/user/dto/jwt-user.dto';
 
 @Injectable()
 export class JWTGuard implements CanActivate {
   constructor(private jwtService: JwtService) {}
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    const req: Request<
-      any,
-      {
-        id?: number;
-      }
-    > = context.switchToHttp().getRequest();
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request: Request = context.switchToHttp().getRequest();
+
     let jwtUser;
 
-    if (req?.cookies?.jwt == null) {
+    if (request?.cookies?.jwt == null) {
       throw new HttpException('로그인 후 이용가능합니다.', 401);
     }
 
     try {
-      jwtUser = this.jwtService.verify<jwtUserDTO>(req.cookies['jwt']);
+      jwtUser = this.jwtService.verify<jwtUserDTO>(request.cookies['jwt']);
     } catch (error) {
       throw new HttpException('JWT 토큰이 만료되었습니다.', 400);
-    }
-
-    if (req.body.id == null) {
-      throw new HttpException('id 값이 존재하지 않습니다.', 403);
-    }
-
-    if (+req.body.id !== +jwtUser.id) {
-      throw new HttpException('본인의 글이 아닙니다.', 403);
     }
 
     return true;
--- daycare-backend/src/user/guard/jwt.guard.ts
+++ daycare-backend/src/user/guard/jwt.guard.ts
@@ -6,35 +6,23 @@ import {
   HttpException,
 } from '@nestjs/common';
 import { Request } from 'express';
-import { Observable } from 'rxjs';
 import { jwtUserDTO } from '../dto/jwt-user.dto';
 
 @Injectable()
 export class JWTGuard implements CanActivate {
   constructor(private jwtService: JwtService) {}
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    const req: Request<
-      any,
-      {
-        id?: number;
-      }
-    > = context.switchToHttp().getRequest();
-    if (req?.cookies?.jwt == null) {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request: Request = context.switchToHttp().getRequest();
+
+    if (request?.cookies?.jwt == null) {
       throw new HttpException('accessToken 값이 올바르지 않습니다.', 401);
     }
-    const jwtUser = this.jwtService.verify<jwtUserDTO>(req.cookies['jwt']);
-
-    if (req.body.id == null) {
-      throw new HttpException('id 값이 존재하지 않습니다.', 403);
-    }
-
-    if (+req.body.id !== +jwtUser.id) {
-      throw new HttpException(
-        'id 값과 accessToken 값이 일치하지 않습니다.',
-        403,
+    try {
+      const jwtUser = this.jwtService.verify<jwtUserDTO>(
+        request.cookies['jwt'],
       );
+    } catch (error) {
+      throw new HttpException('JWT 토큰이 만료되었습니다.', 400);
     }
 
     return true;
